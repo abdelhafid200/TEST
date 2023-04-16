14ab23575cf72bb5fc31abe3f14594b404767022
abdelhafid200 <130927691+abdelhafid200@users.noreply.github.com>
2023-04-16 18:26:09 +0000

    Add files via upload

---
+++ accueil2.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <div class="navbar">
+        <nav class="">
+            <div class="nav1">
+                <div class="logo">
+                    <p style="margin-left: 18px;">Direction Générale de la surete Nationale Service d'élections
+                    Listes elelectrocales génerales </p>
+                </div>
+                <div class="demande">
+                    <p>
+                        Demandes
+                    </p>
+                    <p>
+                        Bureaux du vote
+                    </p>
+                    <p>
+                        Liste électrocales 
+                    </p>
+                    <p>
+                        <input type="submit" value="consulter votre demande" class="consulter">
+                    </p>
+                    <p>
+                        <input type="submit" value="Déposer une demande" class="deposer">
+                    </p>
+                </div>
+            </div>
+        </nav>
+        <nav class="nav2">
+            <h4 style="margin-top: 10px;">Portail du service d'élections nationales</h4>
+        </nav>
+    </div>
+    
+        <h1 class="demande_titre" >Demande n° 3</h1>
+    <div class="info">
+        <div class="personnel">          
+                    <div>
+                        <strong>Informations personnelles</strong>
+                    </div>
+                    <div class="full_name">
+                        <small>nom</small>
+                        <small>prénom</small>
+                        <strong>khalil</strong>
+                        <strong>abdo</strong>
+
+                    </div>
+
+                    <div class="madaniya">
+                        <span>cin</span>
+                        <span>sexe</span>
+                        <span>IA23444</span>
+                        <span>HOMMME</span>
+                    </div>
+
+                    <div class="addr">
+                        <span>Date de naissance</span>
+                        <span>Adresse email</span>
+                        <span>01 Jan 2002</span>
+                        <span>abdokhalil@gmail.COM</span>
+                    </div>
+
+                    <div class="tele">
+                        <span>N° de téléphone</span>
+                        <span>0767036697</span>
+                    </div>
+        </div>
+
+        <div class="date">
+                    <div>
+                        Date de demande
+                    </div>
+                    <div>
+                        <strong>
+                            25 Jan 2023
+                        </strong>
+                    </div>
+        </div>
+
+        <div class="lieu">
+                    <div>
+                        <strong>
+                            Lieu de résidence
+                        </strong>
+                    </div>
+
+                    <div class="prov_comm">
+                        <small>Province</small>
+                        <small>Commune</small>
+                        <strong>KASBA TADLA</strong>
+                        <strong>Béni Mellal-Khénifra</strong>
+                    </div>
+                    <div class="hay">
+                        <small>Adresse</small>
+                        <strong>hay smara N°:26</strong>
+                    </div>
+        </div>
+
+        <div class="accept">
+                    <h4>Accepte la demande</h4>
+                    <p>Date d'expiration</p>
+                    <span><input type="date" name="" id=""></span>
+                    <p><small>La date d'expiration de la carte d'élécteur</small></p>
+                    <p>Bureau du vote</p>
+                    <input type="text" name="" value="Bureaux 3" id="">
+                    <p>
+                    Choisir le bureau pour lélecteur à voter. les bureaux listés ci-dessus sont les buraux dont la province et commune sont confondus à l'électeur
+                    </p>
+                    <input type="submit" value="Valider demande" class="Valider_demande"><br>
+                    <input type="reset" value="Réfuser la demande" class="refuser_demande">
+        </div>
+                
+    </div>
+    
+</body>
+</html>
